e9ff193f601582e5a1a78ef9bf42dc806a3d52c1
Matheussdem <matheussdem65@gmail.com>
2025-08-25 17:20:02 -0300

    Criação de Mateiras e post

--- rotas/Materias.php
+++ rotas/Materias.php
@@ -1,17 +1,28 @@
-<?php
+<?php 
+/* CABEÇALHOS do HTTP */
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control_allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+/*
+Essa variável recebe o metodo utilizado
+pode ser POST, GET, PUT ou DELETE
+*/
 $metodoSolicitado = $_SERVER['REQUEST_METHOD'];
-$id = $_GET['id'] ?? null;
 
+/*Esse Id é quando colocamos informações na URL*/
+$id     = $_GET['id'] ?? null;
+/*
+ ?? significa que se $_GET['id'] existir e não for nula 
+o conteudo entra na variável id
+*/
 switch($metodoSolicitado){
     case "POST":
-        $dados_recebido = json_decode(file_get_contents("php://input"), true);
+        $dados_recebidos = json_decode(file_get_contents("php://input"), true);
         break;
-
-        case "GET":
-            echo "Veio do navegador";
-            break;
+    case "GET":
+        echo "{ 'Veio do navegador': true}";
+        break;    
 }
+
+
 ?>
